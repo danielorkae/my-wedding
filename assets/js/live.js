572d66c550c457ddc2624f2d6e88f038d4e5f1de
Daniel Lopes <danielorkae@outlook.com>
2020-06-27 10:56:02 -0300

    add seconds to countdown

--- assets/js/live.js
+++ assets/js/live.js
@@ -11,15 +11,19 @@
     let days = Math.floor(diffInHours / 24);
     let hours = Math.floor(diffInHours - days * 24);
     let minutes = Math.floor((diffInHours - (days * 24 + hours)) * 60);
+    let seconds = Math.floor(
+      (diffInHours - (days * 24 + hours + minutes / 60)) * 3600
+    );
 
     $(".days .days__value").html(days);
     $(".hours .hours__value").html(hours);
     $(".minutes .minutes__value").html(minutes);
+    $(".seconds .seconds__value").html(seconds);
   };
 
   const init = () => {
     setTimeDiff(weddingDate);
-    setInterval(() => setTimeDiff(weddingDate), 30 * 1000);
+    setInterval(() => setTimeDiff(weddingDate), 1000);
   };
 
   init();
