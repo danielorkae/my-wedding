ea2904b9c2170495585785d21a080616e68c3c26
Daniel Lopes <danielorkae@outlook.com>
2020-07-31 08:44:49 -0300

    add broadcast redirect

--- assets/js/live.js
+++ assets/js/live.js
@@ -1,10 +1,17 @@
 (() => {
   const weddingDate = new Date("2020-08-01 16:00");
+  const broadcastUrl = "https://youtu.be/E1SvzFgRGZY";
+
+  var redirectCounter = 5;
 
   const setTimeDiff = (weddingDate) => {
     let now = new Date();
 
     let diff = weddingDate.getTime() - now.getTime();
+
+    if (diff <= 0) {
+      diff = 0;
+    }
 
     let diffInHours = diff / 1000 / 60 / 60;
 
@@ -21,9 +28,25 @@
     $(".seconds .seconds__value").html(seconds);
   };
 
+  const redirectCountDown = () => {
+    if (redirectCounter <= 0) {
+      redirectToBroadcast();
+      return;
+    }
+
+    redirectCounter--;
+    $(".redirect-counter").html(redirectCounter);
+  };
+
+  const redirectToBroadcast = () => {
+    window.location.href = broadcastUrl;
+  };
+
   const init = () => {
     setTimeDiff(weddingDate);
     setInterval(() => setTimeDiff(weddingDate), 1000);
+
+    setInterval(() => redirectCountDown(redirectCounter), 1000);
   };
 
   init();
